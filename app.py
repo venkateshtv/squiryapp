78f4df2c2c68ad49c32c241e69a7c1873353a79d
Venkatesh <venkateshtv@outlook.com>
2017-12-26 20:48:57 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import os
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('https://squiry.in')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
