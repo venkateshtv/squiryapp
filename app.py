c3ff086bd8f1f914a4dd152e2f426bc3b72e9d96
Venkatesh <venkateshtv@outlook.com>
2017-12-26 22:27:07 +0530

    success redirect

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 
-import os
-from flask import Flask, redirect, url_for
+import os, json
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('https://squiry.in')
+	url = 'https://squiry.in/paymentsuccess'
+	if(request.form is not None):
+		url+='?txnid='+request.form.get('txnid')
+	elif request.data is not None:
+		url+= '?txnid='+json.loads(request.data.txnid)
+		
+    return redirect(url)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
